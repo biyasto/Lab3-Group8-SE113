d0593364d0704cb01c8480c93a4372ffb4883737
biyasto <biyasto@gmail.com>
2021-10-21 22:34:26 +0700

    add testcase

--- UnitTest1/Test.cs
+++ UnitTest1/Test.cs
@@ -7,26 +7,29 @@ namespace Test_day_in_month
     public class TestCase
     {
 
-        [TestCase(1, 2021, 31)]
-        [TestCase(3, 2021, 31)]
-        [TestCase(5, 2021, 31)]
-        [TestCase(7, 2021, 31)]
-        [TestCase(9, 2021, 30)]
-        [TestCase(0,2010,0)]
-        [TestCase(1,0,0)]
-        [TestCase(10, 2021, 31)]
-        [TestCase(12, 2021, 31)]
+    
+        //Lab testcase
+        [TestCase(0, 2000, 0)]
+        [TestCase(13, 2000, 0)]
+
         [TestCase(1, 2000, 31)]
-        [TestCase(9, 2000, 30)]
+        [TestCase(3, 2000, 31)]
+        [TestCase(4, 2000, 30)]
+        [TestCase(5, 2000, 31)]
         [TestCase(6, 2000, 30)]
         [TestCase(7, 2000, 31)]
+        [TestCase(8, 2000, 31)]
+        [TestCase(9, 2000, 30)]
+        [TestCase(10, 2000, 31)]
         [TestCase(11, 2000, 30)]
-        [TestCase(0, 2000, 0)]
+        [TestCase(12, 2000, 31)]
+        
         [TestCase(2, 2000, 29)]
-        [TestCase(2, 2004, 29)]
-        [TestCase(2, 2100, 28)]
         [TestCase(2, 2009, 28)]
-
+        [TestCase(2, 1900, 28)]
+        [TestCase(2, 1904, 29)]
+        [TestCase(2, 1903, 28)]
+       
         public void Test_day_in_month(int month, int year, int result)
         {
             var check = new CheckDayClass();
--- UnitTest2/Test2.cs
+++ UnitTest2/Test2.cs
@@ -6,27 +6,56 @@ namespace Test_Validate
     [TestFixture]
    public class TestCase
         {
-            [TestCase(20, 10, 1999, true)]
-            [TestCase(11, 11, 1, true)]
-            [TestCase(31, 12, 1, true)]
-            [TestCase(1, 13, 1957, false)]
-            [TestCase(29, 2, 2009, false)]
-            [TestCase(29, 1, 2000, true)]
-            [TestCase(10, 1, 2001, true)]
-            [TestCase(18, 8, 2001, true)]
-            [TestCase(28, 3, 2001, true)]
-            [TestCase(28, 4, 2001, true)]
-            [TestCase(28, 5, 2001, true)]
-            [TestCase(28, 6, 2001, true)]
-            [TestCase(28, 7, 2001, true)]
-            [TestCase(-1, 8, 2000, false)]
-            [TestCase(28, 9, 3000, true)]
-            [TestCase(30, 1, 2000, true)]
-            [TestCase(31, 1, 2010, true)]
-            [TestCase(30, 2, 1950, false)]
-            [TestCase(0, 6, 2021, false)]
+            [TestCase(28, 0, 2000, false)]
+            [TestCase(28, 13, 2000, false)]
+            [TestCase(0, 4, 2000, false)]
 
-            public void Test_Validate(int day, int month, int year, bool result)
+            [TestCase(0, 1, 2000, false)]
+            [TestCase(0, 3, 2000, false)]
+            [TestCase(0, 5, 2000, false)]
+            [TestCase(0, 7, 2000, false)]
+            [TestCase(0, 8, 2000, false)]
+            [TestCase(0, 10, 2000, false)]
+            [TestCase(0, 12, 2000, false)]
+
+            [TestCase(31, 1, 2000, true)]
+            [TestCase(31, 3, 2000, true)]
+            [TestCase(31, 5, 2000, true)]
+            [TestCase(31, 7, 2000, true)]
+            [TestCase(31, 8, 2000, true)]
+            [TestCase(31, 10, 2000, true)]
+            [TestCase(31, 12, 2000, true)]
+
+            [TestCase(30, 6, 2000, true)]
+            [TestCase(30, 9, 2000, true)]
+            [TestCase(30, 11, 2000, true)]
+            [TestCase(30, 4, 2000, true)]
+
+            [TestCase(31, 6, 2000, false)]
+            [TestCase(31, 9, 2000, false)]
+            [TestCase(31, 11, 2000, false)]
+            [TestCase(31, 4, 2000, false)]
+
+            [TestCase(29, 2, 2000, true)]
+            [TestCase(29, 2, 1900, false)]
+            [TestCase(28, 2, 1990, true)]
+            [TestCase(29, 2, 1904, true)]
+            [TestCase(28, 2, 2000, true)]
+
+        [TestCase(28, 1, 2000, true)]
+        [TestCase(28, 2, 2000, true)]
+        [TestCase(28, 3, 2000, true)]
+        [TestCase(28, 4, 2000, true)]
+        [TestCase(28, 5, 2000, true)]
+        [TestCase(28, 6, 2000, true)]
+        [TestCase(28, 7, 2000, true)]
+        [TestCase(28, 8, 2000, true)]
+        [TestCase(28, 9, 2000, true)]
+        [TestCase(28, 10, 2000, true)]
+        [TestCase(28, 11, 2000, true)]
+        [TestCase(28, 12, 2000, true)]
+        
+        public void Test_Validate(int day, int month, int year, bool result)
             {
                 var check = new CheckDayClass();
                 Assert.AreEqual(result, check.IsValidDate(day,month, year));
