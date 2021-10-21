a80ec8c17299c662cb986d6fcab8eb586d9211e0
biyasto <biyasto@gmail.com>
2021-10-21 21:08:03 +0700

    Add project files.

---
+++ Function1Testing/CheckDay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FunctionX
+{
+    public class CheckDayClass
+    {
+        public int CheckDayinMonth(int month, int year)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                if ((month % 2 == 1 && month <= 7) || (month % 2 == 0 && month > 7)) { return 31; }
+                if (month == 2)
+                {
+                    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) return 29;
+                    else return 28;
+                }
+                return 30;
+            }
+            return 0;
+        }
+        public bool IsValidDate(int day, int month, int year)
+        {
+            if (month < 1 || month > 12 || day < 1)
+                return false;
+            if (day <= CheckDayinMonth(month, year))
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ UnittestFunction1/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using FunctionX;
+namespace Test_day_in_month
+{
+    [TestFixture]
+    public class TestCase
+    {
+        [TestCase(1, 2021, 31)]
+        [TestCase(3, 2021, 31)]
+        [TestCase(5, 2021, 31)]
+        [TestCase(7, 2021, 31)]
+        [TestCase(9, 2021, 30)]
+        [TestCase(10, 2021, 31)]
+        [TestCase(12, 2021, 31)]
+        [TestCase(1, 2000, 31)]
+        [TestCase(9, 2000, 30)]
+        [TestCase(6, 2000, 30)]
+        [TestCase(7, 2000, 31)]
+        [TestCase(11, 2000, 30)]
+        [TestCase(0, 2000, 0)]
+        [TestCase(2, 2000, 29)]
+        [TestCase(2, 2004, 29)]
+        [TestCase(2, 2100, 28)]
+        [TestCase(2, 2009, 28)]
+
+        public void Test_day_in_month(int month, int year, int result)
+        {
+            var check = new CheckDayClass();
+            Assert.AreEqual(result, check.CheckDayinMonth(month, year));
+        }
+    }
+    
+}
